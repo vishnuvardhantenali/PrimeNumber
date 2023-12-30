efa2ad42ab5f6f14c68a72e048d2f045aecc2ef3
tvishnuvardhan <vishnuvardhantenali@gmail.com>
2023-12-30 09:17:11 +0530

    Add project files.

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumber
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5;
+
+            int count = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                if (n % i == 0)
+                {
+                    count++;
+                }
+            }
+            if (count == 2)
+            {
+                Console.WriteLine("The number {0} is Prime", n);
+            }
+            else
+            {
+                Console.WriteLine("It is not prime");
+            }
+            Console.ReadLine();
+        }
+    }
+}
